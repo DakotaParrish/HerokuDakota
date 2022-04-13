5383c04a0246b720fc5490ddcc68c028240a61d6
Dakota Parrish <needforspeeddj@gmail.com>
2022-04-13 05:56:32 -0400

    Added ICE12 to Heroku

---
+++ Server/Config/db.ts
@@ -0,0 +1,6 @@
+//Test comment
+export const LocalURI="mongodb://127.0.0.1:27017/contacts";
+export const RemoteURI="mongodb+srv://dakotap:ClaireJ0331@cluster0.85k1a.mongodb.net/contacts?retryWrites=true&w=majority"
+export const SessionSecret = "WEBD6201SessionSecret";
+//export const Host = "localhost";
+export const Host = "MongoDB Atlas";
